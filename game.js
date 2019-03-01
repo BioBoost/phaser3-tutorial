a2e605454a5406578dfa344a9999ec4f9aa61f5f
Nico De Witte <nico.dewitte@outlook.com>
2019-03-01 08:25:27 +0100

    Load assets and display some

--- game.js
+++ game.js
@@ -12,9 +12,26 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload () {
+  // Assets are accessible via asset key: sky, ground, ...
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('ground', 'assets/platform.png');
+  this.load.image('star', 'assets/star.png');
+  this.load.image('bomb', 'assets/bomb.png');
+
+  // Spritesheet of the player character
+  this.load.spritesheet('dude', 
+      'assets/dude.png',
+      { frameWidth: 32, frameHeight: 48 }
+  );
 }
 
 function create () {
+  // Center of asset is anchor by default
+  // Topleft of screen = (0, 0)
+  // Anchor can be moved using setOrigin
+  this.add.image(0, 0, 'sky').setOrigin(0, 0);
+
+  this.add.image(400, 300, 'star');   // Center of screen
 }
 
 function update () {

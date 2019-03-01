4f82f3976fc15a412188583a90c1d7d76a95624c
Nico De Witte <nico.dewitte@outlook.com>
2019-03-01 08:56:56 +0100

    Spawn player

--- game.js
+++ game.js
@@ -49,6 +49,34 @@ function create () {
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
   platforms.create(-184, 45, 'ground').setOrigin(0, 0);
+
+  // Creating a player
+  player = this.physics.add.sprite(100, 450, 'dude');
+  player.setBounce(0.2);
+
+  // Stop from falling through bottom of scene
+  // Does not stop from falling through platforms
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+  });
+
+  this.anims.create({
+      key: 'turn',
+      frames: [ { key: 'dude', frame: 4 } ],
+      frameRate: 20
+  });
+
+  this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+  });
 }
 
 function update () {
